2499061893e1c49ceb4c10522fdef02eee7da6b1
nkhatri@calibo.com <nkhatri@calibo.com>
2024-07-30 14:35:31 +0000

    Update ApplicationTests.java

--- src/test/java/com/template/demo/ApplicationTests.java
+++ src/test/java/com/template/demo/ApplicationTests.java
@@ -1,10 +1,15 @@
 package com.template.demo;
 
+import com.template.demo.controller.DefaultControllerTests;
+import com.template.demo.controller.ServiceControllerTests;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ContextConfiguration(classes = {ServiceControllerTests.class, DefaultControllerTests.class})
 public class ApplicationTests {
 
 	@Test
